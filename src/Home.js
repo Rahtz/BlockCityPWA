19978f93761900419ae0840dc1fb902a2be63104
Jacob Ratima <jacobratima@gmail.com>
2022-10-28 12:59:17 +1300

    Week games

--- src/Home.js
+++ src/Home.js
@@ -5,29 +5,55 @@ import Highflyers from "./images/Highflyers.png";
 const Home = () => {
   return (
     <div className="-mt-5 bg-gray-200 h-screen">
-
-        <div className="container relative top-10 w-screen h-40 overflow-x-auto scrollbar-hide">
-                <div className="grid grid-cols-2 w-[112px] h-full ml-[30px] bg-gray-800 rounded-lg">
-                    <img className="ml-[5px] mt-[30px] col-span-1 w-[39px] h-[39px]" src={Blockcity} alt="BC"/>
-                    <img className="-ml-[2px] mt-[30px] col-span-1 w-[65px] h-[39px]" src={Highflyers} alt="HF"/>
-                    {/* <div className="w-[112px]">
-                        <h1 className="text-white text-sm">Block City<span className="pl-5">98</span></h1>
-                        <h1 className="text-white text-sm">High Flyers<span className="pl-5">69</span></h1>
-                    </div> */}
-                    <table className="w-[112px]">
-                        <tr>
-                            <th className="text-white text-[12px]">Block City</th>
-                            <th className="text-white text-[12px] pr-[5px]">98</th>
-                        </tr>
-                        <tr>
-                            <th className="text-white text-[12px]">High Flyers</th>
-                            <th className="text-white text-[12px] pr-[5px]">69</th>
-                        </tr>
-                    </table>
-                </div>
-                <div className="w-[112px] h-full ml-[172px] -mt-40 bg-blue-500 rounded-lg"></div>
-                <div className="w-[112px] h-full ml-[314px] -mt-40 bg-blue-500 rounded-lg"></div>
-        </div>
+        
+            <div className="flex container relative top-10 w-screen h-[200px] overflow-x-auto scrollbar-hide pt-12">
+                    <div className="-mt-11">
+                        <h1 className="pl-7 pb-5">Week 1</h1>
+                        <div className="grid grid-cols-2 w-[112px] h-[150px] ml-[30px] bg-gray-800 rounded-lg">
+                            <img className="ml-[5px] mt-[30px] col-span-1 w-[39px] h-[39px]" src={Blockcity} alt="BC"/>
+                            <img className="-ml-[2px] mt-[30px] col-span-1 w-[65px] h-[39px]" src={Highflyers} alt="HF"/>
+                            {/* <div className="w-[112px]">
+                                <h1 className="text-white text-sm">Block City<span className="pl-5">98</span></h1>
+                                <h1 className="text-white text-sm">High Flyers<span className="pl-5">69</span></h1>
+                            </div> */}
+                            <table className="w-[112px]">
+                                <tr>
+                                    <th className="text-white text-[12px]">Block City</th>
+                                    <th className="text-white text-[12px] pr-[5px]">98</th>
+                                </tr>
+                                <tr>
+                                    <th className="text-white text-[12px]">High Flyers</th>
+                                    <th className="text-white text-[12px] pr-[5px]">69</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="w-[112px] h-[150px] ml-[172px] -mt-[150px] bg-blue-500 rounded-lg"></div>
+                        <div className="w-[112px] h-[150px] ml-[314px] -mt-[150px] bg-blue-500 rounded-lg"></div>
+                    </div>
+                    <div className="-mt-11">
+                        <h1 className="pl-7 pb-5">Week 2</h1>
+                        <div className="grid grid-cols-2 w-[112px] h-[150px] ml-[30px] bg-gray-800 rounded-lg">
+                            <img className="ml-[5px] mt-[30px] col-span-1 w-[39px] h-[39px]" src={Blockcity} alt="BC"/>
+                            <img className="-ml-[2px] mt-[30px] col-span-1 w-[65px] h-[39px]" src={Highflyers} alt="HF"/>
+                            {/* <div className="w-[112px]">
+                                <h1 className="text-white text-sm">Block City<span className="pl-5">98</span></h1>
+                                <h1 className="text-white text-sm">High Flyers<span className="pl-5">69</span></h1>
+                            </div> */}
+                            <table className="w-[112px]">
+                                <tr>
+                                    <th className="text-white text-[12px]">Block City</th>
+                                    <th className="text-white text-[12px] pr-[5px]">98</th>
+                                </tr>
+                                <tr>
+                                    <th className="text-white text-[12px]">High Flyers</th>
+                                    <th className="text-white text-[12px] pr-[5px]">69</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="w-[112px] h-[150px] ml-[172px] -mt-[150px] bg-blue-500 rounded-lg"></div>
+                        <div className="w-[112px] h-[150px] ml-[314px] -mt-[150px] bg-blue-500 rounded-lg"></div>
+                    </div>
+            </div>
 
         <div className="relative top-16 mx-5 h-auto py-5 border bg-white rounded-lg drop-shadow-lg">
             {/* <table>
