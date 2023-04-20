524d35e50c26a748384479edd720e4e260776ecf
Jacob Ratima <jacobratima@gmail.com>
2023-04-20 16:52:14 +1200

    Added Mobile functionality to add players

--- src/Players.js
+++ src/Players.js
@@ -151,7 +151,7 @@ function Players() {
   return (
     <div className="lg:grid divide-x mt-1">
         <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full 
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-[370px] 
       sm:w-auto px-5 py-2.5 mb-5 mx-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={handleCreateClick}
         >
@@ -193,13 +193,13 @@ function Players() {
                 </th>
                 <td className="py-4 px-6">
                   <button
-                    className="hidden lg:block font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    className="lg:block font-medium text-blue-600 dark:text-blue-500 hover:underline"
                     onClick={() => handleEditClick(player)}
                   >
                     Edit
                   </button>
                   <button
-                    className="hidden lg:block font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    className="lg:block font-medium text-blue-600 dark:text-blue-500 hover:underline"
                     onClick={() => deletePlayer(player.id)}
                   >
                     Delete
@@ -214,7 +214,7 @@ function Players() {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-100 z-50">
           <div className="bg-white rounded-lg p-8 h-5/6 w-auto">
           <h2 className="text-lg font-medium mb-4">Create Player </h2>
-          <label className="flex justify-between mb-2">
+          <label className="flex justify-between">
               <h3>Name:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5 w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -225,7 +225,7 @@ function Players() {
               }
             />
             </label>
-            <div className="flex justify-between mb-2">
+            <div className="flex justify-between">
               <h3>Team:</h3>
             <form
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5  w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -243,7 +243,7 @@ function Players() {
   </select>
             </form>
             </div>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Birthdate:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5  w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -255,7 +255,7 @@ function Players() {
               }
             />
             </label>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Position:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5  w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -266,7 +266,7 @@ function Players() {
               }
             />
             </label>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Number:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5  w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -275,7 +275,7 @@ function Players() {
               onChange={(e) => setPlayer({ ...player, number: e.target.value })}
             />
             </label>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Highschool:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5 w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -286,7 +286,7 @@ function Players() {
               }
             />
             </label>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Weight:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5 w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -295,7 +295,7 @@ function Players() {
               onChange={(e) => setPlayer({ ...player, weight: e.target.value })}
             />
             </label>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Feet:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5 w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -306,7 +306,7 @@ function Players() {
               }
             />
             </label>
-            <label className="flex justify-between mb-2">
+            <label className="flex justify-between">
               <h3>Inches:</h3>
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mb-5 mx-5 w-[200px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -317,15 +317,15 @@ function Players() {
               }
             />
             </label>
-            <div className="button-container">
+            <div className="flex justify-between">
             <button
-                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mb-5 mx-5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-[120px] sm:w-auto px-5 py-2.5 mb-1 mx-5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                 onClick={() => setShowCreate(false)}
               >
                 Cancel
               </button>
             <button
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mb-5 mx-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-[120px] sm:w-auto px-5 py-2.5 mx-5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={createPlayer}
             >
               Create Player
